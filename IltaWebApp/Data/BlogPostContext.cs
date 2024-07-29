5c86b9675a6616b5b84ee72379bfac557d680e01
David <Davidkjensonii@gmail.com>
2024-07-28 19:16:43 -0700

    updated

---
+++ IltaWebApp/Data/BlogPostContext.cs
@@ -0,0 +1,18 @@
+﻿using IltaWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IltaWebApp.Data;
+
+
+    public class BlogPostContext : DbContext
+    {
+        public BlogPostContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    
+}
+
+
+
+
